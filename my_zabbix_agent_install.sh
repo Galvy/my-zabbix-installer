d3ea004959808cccff3015d5bb5dd3a0b2e7b4e0
Galvy <galvani.cristian@gmail.com>
2020-06-08 10:40:32 +0200

    Update my_zabbix_agent_install.sh

--- my_zabbix_agent_install.sh
+++ my_zabbix_agent_install.sh
@@ -24,17 +24,19 @@ if [ -x /usr/bin/yum ]; then
                 #echo $CENTOS_VER;
                 /usr/bin/rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
                 /usr/bin/yum clean all
-                /usr/bin/yum install zabbix-agent
+                /usr/bin/yum -y install zabbix-agent
                 /usr/bin/systemctl enable zabbix-agent
-                /usr/bin/sed -i 's/Server=127.0.0.1/Server=192.168.4.213/' /etc/zabbix/zabbix_agentd.conf
-                /usr/bin/sed -i
+                /usr/bin/curl -o /etc/zabbix/zabbix_agentd.conf https://raw.githubusercontent.com/Galvy/my-zabbix-installer/master/zabbix_agentd.conf
+                /usr/bin/systemctl start zabbix-agent
         fi
-#  yum -y update
-#  rpm -ivh http://repo.zabbix.com/zabbix/2.4/rhel/6/x86_64/zabbix-release-2.4-1.el6.noarch.rpm
-#  yum -y install zabbix-agent
-#  chkconfig zabbix-agent on
-#  sed -i 's/Server=127.0.0.1/Server=52.90.49.206/' /etc/zabbix/zabbix_agentd.conf
-#  sed -i 's/ServerActive=127.0.0.1/ServerActive=52.90.49.206/' /etc/zabbix/zabbix_agentd.conf
-#  HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
-#  service zabbix-agent restart
+        if [ "$CENTOS_VER" -eq "8" ]; then
+                #echo $CENTOS_VER;
+                /usr/bin/rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/8/x86_64/zabbix-release-5.0-1.el8.noarch.rpm
+                /usr/bin/dnf clean all
+                /usr/bin/yum -y install zabbix-agent
+                /usr/bin/systemctl enable zabbix-agent
+                /usr/bin/curl -o /etc/zabbix/zabbix_agentd.conf https://raw.githubusercontent.com/Galvy/my-zabbix-installer/master/zabbix_agentd.conf
+                /usr/bin/systemctl start zabbix-agent
+        fi
+
 fi
